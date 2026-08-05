8effd91f7eead732fe306c295e7ddedc25b98082
Justin <justinsotoramos2005@gmail.com>
2026-08-05 14:35:30 -0500

    Correcion

--- Taller Refactoring/src/EmpleadoTemporario.java
+++ Taller Refactoring/src/EmpleadoTemporario.java
@@ -2,14 +2,20 @@ public class EmpleadoTemporario extends Empleado {
     private int mesesContrato;
 
     public EmpleadoTemporario(String nombre, double salarioBase, int horasTrabajadas, String departamento, int mesesContrato, String genero) {
-        super(nombre, salarioBase, horasTrabajadas, 0, departamento);
+        super(nombre, salarioBase, horasTrabajadas, 0.0, departamento, genero);
         this.mesesContrato = mesesContrato;
     }
 
-public String obtenerDetalles() {
-    return String.format("Nombre: %s\nGénero: %s\nSalario: %.2f\nHoras trabajadas: %d\nDepartamento: %s\nMeses de contrato: %d",
-                getNombre(), getGenero(), getSalarioBase(), getHorasTrabajadas(), getDepartamento(), mesesContrato);
+    public int getMesesContrato() {
+        return mesesContrato;
     }
 
-    // Más metodos
+    public void setMesesContrato(int mesesContrato) {
+        this.mesesContrato = mesesContrato;
+    }
+
+    public String obtenerDetalles() {
+        return String.format("Nombre: %s\nGénero: %s\nSalario: %.2f\nHoras trabajadas: %d\nDepartamento: %s\nMeses de contrato: %d",
+                getNombre(), getGenero(), getSalarioBase(), getHorasTrabajadas(), getDepartamento(), mesesContrato);
+    }
 }
