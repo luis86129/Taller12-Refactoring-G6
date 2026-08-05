6e9e187711f08ef8fee78be421a835788dc872ad
Justin <justinsotoramos2005@gmail.com>
2026-08-05 14:29:22 -0500

    Actualizacion de Empleado

--- Taller Refactoring/src/Empleado.java
+++ Taller Refactoring/src/Empleado.java
@@ -4,14 +4,16 @@ public class Empleado {
     private int horasTrabajadas;
     private String departamento;
     private double tarifaHora;
+    private String genero;
 
     public Empleado(){}
-    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento) {
+    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento, String genero) {
         this.nombre = nombre;
         this.salarioBase = salarioBase;
         this.horasTrabajadas = horasTrabajadas;
         this.tarifaHora = tarifaHora;
         this.departamento = departamento;
+        this.genero = genero;
     }
 
     public double calcularSalario() {
@@ -81,5 +83,13 @@ public class Empleado {
         this.departamento = departamento;
     }
 
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+
     // Más metodos
+    }
 }
